f1be6cdcd284cfdbf5d7dc3c0556d9a0cbbc7f25
Robin Nunkesser <robin.nunkesser@hshl.de>
2021-03-16 16:00:33 +0100

    Fix CI

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 cd TuneSearch
-xcodebuild test -enableCodeCoverage YES -scheme TuneSearch -sdk iphonesimulator \
+xcodebuild test -enableCodeCoverage YES -workspace TuneSearch.xcworkspace \
+  -scheme TuneSearch -sdk iphonesimulator \
   -destination 'platform=iOS Simulator,name=iPhone 11' ONLY_ACTIVE_ARCH=NO
